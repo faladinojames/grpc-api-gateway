edfb60d757a2bb5d3149d419cd98c75bd7ebea26
James Falade <jfalade@terragonltd.com>
2020-03-22 21:21:22 +0100

    added profile service

--- compile_pb.sh
+++ compile_pb.sh
@@ -2,4 +2,6 @@ protoc \
         --include_imports \
         --include_source_info \
         --descriptor_set_out out.pb \
-        pb/authentication.proto pb/gateway.proto pb/payment.proto pb/trip.proto pb/driver-authentication.proto
+        pb/authentication.proto pb/gateway.proto pb/payment.proto pb/trip.proto pb/driver-authentication.proto pb/profile.proto
+
+gcloud endpoints services deploy out.pb api_config.yaml
