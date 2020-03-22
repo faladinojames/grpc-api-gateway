e6ccf3aedee7b16455ac9689240accac5c2c5343
James Falade <jfalade@terragonltd.com>
2020-03-22 08:44:23 +0100

    attempt proxy metadata fix

--- compile_pb.sh
+++ compile_pb.sh
@@ -2,4 +2,4 @@ protoc \
         --include_imports \
         --include_source_info \
         --descriptor_set_out out.pb \
-        pb/authentication.proto pb/gateway.proto pb/payment.proto pb/trip.proto
+        pb/authentication.proto pb/gateway.proto pb/payment.proto pb/trip.proto pb/driver-authentication.proto
